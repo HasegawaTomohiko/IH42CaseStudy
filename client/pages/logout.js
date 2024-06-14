16d2d93a3605f6e639177990e7e59ea833685a45
o-ken-shin0428 <takenobu3710@gmail.com>
2024-06-14 14:23:12 +0900

    sideMenu制作

---
+++ client/pages/logout.js
@@ -0,0 +1,8 @@
+import SampleSideMenu from "@/components/layout/sideMenu";
+    export default function Logout() {
+        return (
+          <>
+              <SampleSideMenu menutitle="ログアウト"/>
+          </>
+        )
+      }
