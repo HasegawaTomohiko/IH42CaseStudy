46602349033a6b9acf11cc498143d189f4c06fd6
hrking575757 <basket.haru57@gmail.com>
2024-06-24 09:31:20 +0900

    ri

--- client/pages/logout.js
+++ client/pages/logout.js
@@ -1,8 +1,10 @@
 import SampleSideMenu from "@/components/layout/sideMenu";
+import InputWithIcon from "@/components/dashboard/logout";
     export default function Logout() {
         return (
           <>
               <SampleSideMenu menutitle="ログアウト"/>
+              <InputWithIcon />
           </>
         )
       }
