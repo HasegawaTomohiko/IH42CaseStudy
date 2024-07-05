49dd411d303f5d8f9d18e2f3b9109753ca455248
近藤千瑛 <kondo.chi@outlook.jp>
2024-07-05 16:04:06 +0900

    新規登録

--- client/components/register/index.js
+++ client/components/register/index.js
@@ -3,22 +3,38 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+import styles from "./register.module.scss";
+
+console.log(styles);
 
 export default function FormPropsTextFields() {
   return (
     <Box
       component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
-      }}
+      className={styles.container}
       noValidate
       autoComplete="off"
     >
+
+    <div className={styles.fieldContainer}>
+    <Typography variant="h5" className={styles.typography}>
+      ドライバーID
+    </Typography>
+
         <TextField
           required
           id="DoriverId"
           label="ドライバーID"
+          className={styles.textField}
         />
+    </div>
+
+    <div className={styles.fieldContainer}>
+    <Typography variant="h5" className={styles.typography}>
+      パスワード
+    </Typography>
 
         <TextField
           required
@@ -26,7 +42,14 @@ export default function FormPropsTextFields() {
           label="パスワード"
           type="password"
           autoComplete="current-password"
+          className={styles.textField}
         />
+    </div>
+
+    <div className={styles.fieldContainer}>
+    <Typography variant="h5" className={styles.typography}>
+      もう一度パスワードを入力してください
+    </Typography>
             
         <TextField
           required
@@ -34,10 +57,12 @@ export default function FormPropsTextFields() {
           label="パスワード"
           type="password"
           autoComplete="current-password"
+          className={styles.textField}
         />
+    </div>
 
         <Stack spacing={2} direction="row">
-            <Button variant="contained" href='map'>登録</Button>
+            <Button variant="contained" className={styles.button} href='map'>登録</Button>
         </Stack>
 
     </Box>
